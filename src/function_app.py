4c489c052627cc0d509f04f03bbc94d408d46bd1
Mahra Rahimi <mahra.rah@gmail.com>
2024-08-23 14:11:16 +0200

    feat: add instrumentation

--- src/function_app.py
+++ src/function_app.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 import azure.functions as func
 from azure.monitor.opentelemetry import configure_azure_monitor
 
@@ -5,9 +8,16 @@ from monitor.monitoring_job import queue_metrics_monitor_blueprint
 from routes.config import config_bp
 from routes.health import health_bp
 
-# Configure OpenTelemetry to use Azure Monitor with the
-# APPLICATIONINSIGHTS_CONNECTION_STRING environment variable.
-configure_azure_monitor()
+# root_logger = logging.getLogger()
+# for handlers in root_logger.handlers[:]:
+#     root_logger.removeHandler(handlers)
+
+logging.getLogger("azure.core").setLevel(logging.WARNING)
+
+
+configure_azure_monitor(
+    connection_string=os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 app.register_functions(health_bp)
